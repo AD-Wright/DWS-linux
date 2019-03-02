f737b6b66d357748cf14f8d6380f0e1b2bd0d0a8
AD-Wright <adw@wrightsahead.com>
2019-03-01 22:47:09 -0700

    Added directory for alternative program, based solely on a bash script, perhaps activated by cron

---
+++ Script_Only_Mode/Script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+//use cron to run every few minutes
+//check if file has changed "wget"
+//if has changed, run alert("notify-send"?), then find major color of image using "convert" from imageMajic
+//use "yan" to manage GTK icon in systray and menu, maybe also alerts?
