6012f556833208b2b290041b7e19a48b3fa3c658
AD-Wright <adw@wrightsahead.com>
2019-08-13 11:55:53 -0600

    Account for rsstail sometimes crashing

--- twitter.sh
+++ twitter.sh
@@ -24,6 +24,8 @@ DWS_URL="https://queryfeed.net/tw?token=5d51b243-c413-4266-8235-c7e30a4a28f7&q=%
 
 # END USER CONFIG
 
+while true; do
+
 # kill existing rsstail processes (change if you use for other things)
 pkill rsstail
 
@@ -31,3 +33,5 @@ pkill rsstail
 rsstail -i $UPDATE_INT -u $DWS_URL -n 0 -dNH | ( read -r T1 T2; read -r T3; yad --title="DWS_linux" --text="News: $T3 $T5" --wrap --geometry=300x150 --show-uri; )
 
 #rsstail -i 300 -u "https://queryfeed.net/tw?token=5d51b243-c413-4266-8235-c7e30a4a28f7&q=%40DEFCONWSALERTS" -n 1 -dNH 
+
+done;
