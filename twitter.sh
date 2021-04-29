e268aa13ab8657272af7fa3b8ca88b31f6616b80
AD-Wright <adw@wrightsahead.com>
2021-04-28 19:11:32 -0600

    Big changes:  - Changed notifier to notify-send  - Removed rsstail dependency by doing own parsing  - Changed default url to one that should be more reliable

--- twitter.sh
+++ twitter.sh
@@ -8,9 +8,12 @@
 #                                                                             #
 ###############################################################################
 
-#THIS IS NOT THE MAIN FILE
+# THIS IS NOT THE MAIN FILE
 # This is a helper script, used to provide the twitter notifications through
-# notify-send and nitter (nitter provides rss feeds for twitter).
+# notify-send and an rss feed.  This feed is hosted by me, and may briefly go
+# down if I move apartments.  If desired, the feed url can be pointed at an rss
+# provider of your choice, I have used nitter in the past, but it sometimes 
+# seems to error out.
 # This file does not need to be autostarted, but it should have exec permission
 # in order to be properly triggered by the DWS-linux main script.
 
@@ -18,22 +21,41 @@
 # installed directory (update after installation)
 INSTALL_DIR=~/Documents/DWS-linux
 
-# update interval: default 300 (5 minutes)
-UPDATE_INT=500
+# update interval: default 300 (5 minutes in seconds)
+# (I provide the default feed, please do not spam with updates)
+UPDATE_INT=300
 
 # rss feed url for DEFCONWSALERTS
-DWS_URL="https://nitter.net/DEFCONWSALERTS/rss"
+DWS_URL="https://defcon.ironeagl.com/dws-linux.xml"
 
 # END USER CONFIG
 
+# Check last rss.xml download
+TITLE=$( grep -som2 '<title>[^<]*' $INSTALL_DIR/dws-linux.xml | tail -n1 | grep -so '[^>]*$')
+LINK=$( grep -som2 '<link>[^<]*' $INSTALL_DIR/dws-linux.xml | tail -n 1 | grep -so '[^>]*$')
+DESCRIPTION=$( grep -som2 '<description>[^<]*' $INSTALL_DIR/dws-linux.xml | tail -n 1 | grep -so '[^>]*$')
+
 while true; do
 
-# kill existing rsstail processes (change if you use for other things)
-pkill rsstail
+# Re-download rss.xml (if file has changed on server)
+wget -Nq $DWS_URL
 
-# use rsstail to check the twitter page, pipe output to yad with some formatting
-rsstail -i $UPDATE_INT -u $DWS_URL -n 0 -dNHP | ( read -r T1 T2; read -r T3; notify-send -u critical -i $INSTALL_DIR/images/current.png "DWS_linux" "News: $T1 $T2"; )
+# Grab new message link (links are unique for tweets)
+NEW_LINK=$( grep -som2 '<link>[^<]*' $INSTALL_DIR/dws-linux.xml | tail -n 1 | grep -so '[^>]*$')
 
-#rsstail -i 300 -u "https://nitter.net/DEFCONWSALERTS/rss" -n 1 -dNHP | ( read -r T1 T2; read -r T3; notify-send -u critical -i ~/Documents/DWS-linux/images/current.png "DWS_linux" "News: $T1 $T2"; )
+# If not the same, then display popup using notify-send
+if [[ "$NEW_LINK" != "$LINK" ]]; then
+    TITLE=$( grep -som2 '<title>[^<]*' $INSTALL_DIR/dws-linux.xml | tail -n1 | grep -so '[^>]*$')
+    LINK=$( grep -som2 '<link>[^<]*' $INSTALL_DIR/dws-linux.xml | tail -n1 | grep -so '[^>]*$')
+    DESCRIPTION=$( grep -som2 '<description>[^<]*' $INSTALL_DIR/dws-linux.xml | tail -n1 | grep -so '[^>]*$')
+    # Convert some html number codes to prettify
+    TITLE=$( echo $TITLE | sed 's/&amp;/\&/g; s/&lt;/\</g; s/&gt;/\>/g; s/&quot;/\"/g; s/&#39;/\'"'"'/g;' )
+    DESCRIPTION=$( echo $DESCRIPTION | sed 's/&amp;/\&/g; s/&lt;/\</g; s/&gt;/\>/g; s/&quot;/\"/g; s/&#39;/\'"'"'/g;' )
+    # Send the notification
+    notify-send -u critical -i $INSTALL_DIR/images/current.png "$TITLE" "$DESCRIPTION \n $LINK"
 
+# Else, wait for UPDATE_INT
+else
+    sleep $UPDATE_INT
+fi
 done;
